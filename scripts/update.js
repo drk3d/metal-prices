7e5653baea0678708e1c19c5b78024a879b4482d
Drakon3D <hello@drakon3d.com>
2026-07-11 16:29:17 +0100

    First updater test

--- scripts/update.js
+++ scripts/update.js
@@ -1,1 +1,14 @@
+console.log("==================================");
+console.log(" Drakon Metal Prices Updater");
+console.log("==================================");
+console.log("");
 
+console.log("Workflow started.");
+
+const now = new Date();
+
+console.log("Current UTC Time:", now.toISOString());
+
+console.log("");
+
+console.log("Everything is working!");
